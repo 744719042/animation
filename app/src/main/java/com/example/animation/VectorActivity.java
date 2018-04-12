d7bc9e6d927da8f49211872ef745510633ecfaf6
张兴忠 <zxz@meitu.com>
2018-04-12 20:53:16 +0800

    添加SVG动画

--- app/src/main/java/com/example/animation/VectorActivity.java
+++ app/src/main/java/com/example/animation/VectorActivity.java
@@ -1,12 +1,23 @@
 package com.example.animation;
 
 import android.graphics.drawable.AnimatedVectorDrawable;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.ImageView;
 
 public class VectorActivity extends AppCompatActivity {
     private ImageView imageView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +25,21 @@ public class VectorActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image);
         AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) imageView.getDrawable();
         drawable.start();
+
+        imageView2 = (ImageView) findViewById(R.id.image2);
+        AnimatedVectorDrawable drawable2 = (AnimatedVectorDrawable) imageView2.getDrawable();
+        drawable2.start();
+
+        imageView3 = (ImageView) findViewById(R.id.image3);
+        AnimatedVectorDrawable drawable3 = (AnimatedVectorDrawable) imageView3.getDrawable();
+        drawable3.start();
+
+        imageView4 = (ImageView) findViewById(R.id.image4);
+        AnimatedVectorDrawable drawable4 = (AnimatedVectorDrawable) imageView4.getDrawable();
+        drawable4.start();
+
+        imageView5 = (ImageView) findViewById(R.id.image5);
+        AnimatedVectorDrawable drawable5 = (AnimatedVectorDrawable) imageView5.getDrawable();
+        drawable5.start();
     }
 }
